fbcafb891d1c99feac09c5feb87d7d38dac3a180
yxxyn20 <dbtndus0527@gmail.com>
2022-03-16 01:31:38 +0900

    ch06 실습

--- ch01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ ch01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,19 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) // 부모가 가지고 있는 oncreate 함수를 그대로 사용
 
+        /*
         setContentView(R.layout.activity_main) // xml 파일을 보여줌
+
         // val tv1 = findViewById(R.id.textView)
         val tv1 : TextView = findViewById(R.id.textView)
-
+        val rbar: RatingBar = findViewById(R.id.ratingBar)
+        val btn : Button = findViewById(R.id.button)
+        val chb : CheckBox = findViewById(R.id.checkBox)
+        val rdo : RadioButton = findViewById(R.id.radioButton)
+        //findViewById를 너무 많이 사용함 => 뷰 바인딩 사용
         tv1.visibility = View.INVISIBLE
+
+        */
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.visibility = View.INVISIBLE
+        binding.button.visibility = View.INVISIBLE
+
         Log.d("myCheck", "안드로이드 시작 - 로그 출력")
     }
 }
