37e4ccb8049fbf99c8ad8e5374aafe09a7ce584a
yxxyn20 <dbtndus0527@gmail.com>
2022-04-13 04:30:59 +0900

    ch13 레이아웃 구성하기: 리사이클러 뷰, 플로팅 액션 버튼 생성

--- ch13/app/src/main/java/com/example/ch13/MainActivity.kt
+++ ch13/app/src/main/java/com/example/ch13/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ch13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ch13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas:MutableList<String>? = null
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.mainRecyclerView.adapter = MyAdapter(datas)
     }
 }
