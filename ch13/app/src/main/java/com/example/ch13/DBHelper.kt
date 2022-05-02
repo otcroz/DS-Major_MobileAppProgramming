ead8906d5f6a2734a45672b925ea9b9983e540d9
yxxyn20 <dbtndus0527@gmail.com>
2022-05-03 03:41:26 +0900

    #1 ch17: SQLite를 사용하여 DB 테이블 만들기

--- ch13/app/src/main/java/com/example/ch13/DBHelper.kt
+++ ch13/app/src/main/java/com/example/ch13/DBHelper.kt
@@ -5,12 +5,13 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, "testdb", null, 1) {
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(p0: SQLiteDatabase?) { // DB 테이블 만드는 작업
+        p0?.execSQL("create table todo_tb (_id integer primary key autoincrement, todo not null )") // sql db language
+        p0?.execSQL("create table todo_tb ("+"_id integer primary key autoincrement,"+" todo not null )") // sql db language: 교재에 있는
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+
     }
 
 }
