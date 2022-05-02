274506824e60e5cfa0338d804d227f0a686ea117
yxxyn20 <dbtndus0527@gmail.com>
2022-05-03 03:16:43 +0900

    ch16: 갤러리, 카메라, 지도, 전화앱 연동하기 - 갤러리 앱 연동 - 카메라 앱 연동: 카메라 촬영 후 썸네일 설정, 카메라 촬영 후 photoURI를 통해 썸네일 불러내기 - 지도맵 연동하기 - 전화앱 연동하기: 전화 권한 설정

--- ch16/app/src/main/java/com/example/ch16/MainActivity.kt
+++ ch16/app/src/main/java/com/example/ch16/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.ch16
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.ContactsContract
 import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.ch16.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var filePath:String // 파일의 절대 주소 : 나중에 정의
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -81,8 +90,66 @@ class MainActivity : AppCompatActivity() {
             requestGalleryLauncher.launch(intent) // 인텐트를 전달
         }
 
-        // 사용자에 대한 연락처의 정보를 가져옴
+        val requestCameraThumnailLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            val bitmap = it?.data?.extras?.get("data") as Bitmap // 비트맵을 가져오기
+            binding.userIDImg.setImageBitmap(bitmap) // 이미지 비트맵을 넣기
+        }
+        binding.cameraBtn1.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            requestCameraThumnailLauncher.launch(intent)
+        }
 
+        val requestCameraFileLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            val calRatio = calculateInSampleSize(Uri.fromFile(File(filePath)), 100, 150) // 크기를 위한 비율값 가져오기
+            val option = BitmapFactory.Options()
+            option.inSampleSize = calRatio
+            val bitmap = BitmapFactory.decodeFile(filePath, option) // filepath를 통해 데이터를 가져옴
+            bitmap?.let{
+                binding.userIDImg.setImageBitmap(bitmap)
+            } ?: let{
+                Log.d("mobileApp", "bitmap null")
+            }
+
+        }
+        val timeS:String = SimpleDateFormat("yyyymmdd_hHHmmss").format(Date()) // java.util.Date
+        val storeDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File.createTempFile("JPEG_${timeS}_", ".jpg", storeDir) // 저장 형식
+        filePath = file.absolutePath // 파일의 절대주소
+        val photoURI:Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.ch16.fileprovider",
+            file
+        )
+        binding.cameraBtn2.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            // 촬영한 사진을 저장하기 위해
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI) // URI를 이용하여 카메라를 촬영
+            requestCameraFileLauncher.launch(intent)
+        }
+
+        //지도맵 연동하기
+        binding.mapBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.562952, 126.9779451")) // 지도맵을 불러냄, 위치에 대한 정보
+            startActivity(intent)
+        }
+        
+        //전화앱 연동하기
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+
+        }
+        binding.callBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel: 02-911"))
+            val status = ContextCompat.checkSelfPermission(this, "android.permission.CALL_PHONE") // 권한에 대한 확인
+            if(status == PackageManager.PERMISSION_GRANTED){ // 권한을 허용했을 때
+                startActivity(intent)
+            } else{
+                requestPermissionLauncher.launch("android.permission.CALL_PHONE") // 사용자에게 권한 설정을 묻는다.
+            }
+
+        }
     }
 
     private fun calculateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int {
