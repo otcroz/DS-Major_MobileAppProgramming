883e3951b202241bf26be1ff3d76ef079a85a2b8
yxxyn20 <dbtndus0527@gmail.com>
2022-05-11 02:03:32 +0900

    #2 스마트폰 정보 구하기: 리사이클러 뷰에 정보를 담기 위한 기본 설정
    - MyAdapter 생성
    - item_main 생성, 리사이클러 뷰의 1개의 리스트
    - ItemModel 생성, 필요한 정보 가져오기

---
+++ MyApplication18/ch18_network/src/main/java/com/example/ch18_network/ItemModel.kt
@@ -0,0 +1,3 @@
+package com.example.ch18_network
+
+data class ItemModel(val orgnm:String, val orgTlno: String, val orgZipaddr:String)
---
+++ MyApplication18/ch18_network/src/main/java/com/example/ch18_network/MyAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.ch18_network
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ch18_network.databinding.ItemMainBinding
+
+class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root) // 뷰 홀더에 대한 생성
+class MyAdapter(val context: Context, val datas:MutableList<ItemModel>?):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent,false ))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        val model = datas!![position]
+        binding.name.text = model.orgnm
+        binding.tel.text = model.orgTlno
+        binding.addr.text = model.orgZipaddr
+    }
+
+    override fun getItemCount(): Int {
+        return datas?.size ?: 0
+    }
+
+}
