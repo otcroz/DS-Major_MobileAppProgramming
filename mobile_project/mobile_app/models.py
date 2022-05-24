65cf88d7327d0bd823b8a5ad7dc904e6fb03ef5b
yxxyn20 <dbtndus0527@gmail.com>
2022-05-25 00:26:48 +0900

    #5 데이터 모델 구축 및 admin page에 연결하기

--- mobile_project/mobile_app/models.py
+++ mobile_project/mobile_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class humanInfo(models.Model):
+    name = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=3)
+    address = models.TextField()
+    
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta: ordering = ['created']  # 정렬 순서
