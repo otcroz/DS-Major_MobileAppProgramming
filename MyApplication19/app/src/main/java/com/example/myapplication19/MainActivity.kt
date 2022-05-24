891da48d911001b4de6069b3c17786631aef1a56
yxxyn20 <dbtndus0527@gmail.com>
2022-05-24 21:35:08 +0900

    #4 Google Map API: 지도 타입 변경 메뉴 바 만들기, 마커 설정
    1. 메뉴 바 만들기: onCreateOptionsMenu, onOptionsItemSelected 메서드 사용하여 만들기
    2. 마커 설정: MarkerOptions()

--- MyApplication19/app/src/main/java/com/example/myapplication19/MainActivity.kt
+++ MyApplication19/app/src/main/java/com/example/myapplication19/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.myapplication19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     // 구글 맵에 대한 변수 선언
@@ -21,7 +25,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         googleMap = p0 // 구글맵을 변수에 넣는다.
 
         // 지도의 타입을 바꾸어줌: satellite
-        googleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        //googleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
         //특정 위치를 보여준다.(위도, 경도)
         val latLng = LatLng(37.568256, 126.897240)
         val position: CameraPosition = CameraPosition.Builder() // 카메라를 움직여서 설정해놓은 위치로 지도를 이동하도록 한다.
@@ -29,5 +33,31 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .zoom(16f)
             .build()
         googleMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(position))
+
+        // 마커 추가하기
+        val markerOp = MarkerOptions()
+        markerOp.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        markerOp.position(latLng)
+        markerOp.title("월드컵경기장")
+        googleMap?.addMarker(markerOp)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu?.add(0, 1, 0, "위성지도")
+        menu?.add(0, 2, 0, "일반지도")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            1 -> {
+                googleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+            2 -> {
+                googleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+        }
+        return false
     }
 }
